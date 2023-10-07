e864ef3540cd27c51eb3632d03febdf4ef91aeea
Gitanshu sankhla <lusifer0418@gmail.com>
2023-10-07 14:14:49 +0530

    create server

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 // routes
-const routes = require('./routes/index')
+const userRoutes = require('./routes/user')
+const appRoutes = require('./routes/app')
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// routes
+app.use(userRoutes);
+app.use(appRoutes);
 
 app.listen(3000,()=>{
     console.log('server is listening at http://localhost:3000');
