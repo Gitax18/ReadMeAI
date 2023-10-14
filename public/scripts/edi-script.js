8d05acc9e0caa8344e3987a8013defa83e60bd37
Gitanshu sankhla <lusifer0418@gmail.com>
2023-10-15 00:54:37 +0530

    try to add buttons on image

--- public/scripts/edi-script.js
+++ public/scripts/edi-script.js
@@ -1,56 +1,71 @@
 const converter = new showdown.Converter();
+// let imgs;
 
 const ribbonHtml = `
     <div class="img-ribbon">
-        <button class="button" id="align-left"><span class="material-symbols-outlined">format_align_left</span></button>
-        <button class="button" id="align-left"><span class="material-symbols-outlined">format_align_center</span></button>
-        <button class="button" id="align-left"><span class="material-symbols-outlined">format_align_right</span></button>
+        <button class="button-img" id="align-left"><span class="material-symbols-outlined">format_align_left</span></button>
+        <button class="button-img" id="align-left"><span class="material-symbols-outlined">format_align_center</span></button>
+        <button class="button-img" id="align-left"><span class="material-symbols-outlined">format_align_right</span></button>
     </div>
 `   
 
-function createRibbon(img){
+function createRibbon(img, ind, imgs){
     // *** Do Not Change following code
     console.log('Function run');
-
+    console.log(imgs);
     const imgp = img.parentElement;
-    imgp.classList.add()
+    console.log('Image Paragraph:', imgp);
 
-    let imgCont = document.createElement('div');
-    imgCont.style.border = `3px solid black`;
-    imgCont.style.height = `100%`;
-    imgCont.style.width = `100%`;
+    img.style.height = '20rem';
+    imgs[ind] = img;
 
-    imgCont.innerHTML = '';
-    imgCont.appendChild(img);
-    imgCont.innerHTML += ribbonHtml;
+    imgp.style.position = 'relative';
+    imgp.innerHTML += ribbonHtml;
 
-    
+    throw new Error('It works when there is error in function')
+
+    // let imgCont = document.createElement('div');
+    // imgCont.style.border = `3px solid black`;
+    // imgCont.style.height = `100%`;
+    // imgCont.style.width = `100%`;
+
+    // imgCont.innerHTML = '';
+    // imgCont.appendChild(img);
+    // imgCont.innerHTML += ribbonHtml;
+
+    // imgp.innerHTML = '';
+    // imgp.appendChild(img);
+    // imgp.innerHTML += ribbonHtml;
+
+
     // *** Write Your code here
 
-    console.log("************ IMG")
-    console.log(img);
-    console.log("************ IMG-P")
-    console.log(imgp);
-    console.log(imgp.childNodes); // this must not be empty, but is empty, WHY?
-    console.log("************ IMG-Cont")
-    console.log(imgCont);
+
+    // console.log("************ IMG")
+    // console.log(img);
+    // console.log("************ IMG-P")
+    // console.log(imgp);
+    // console.log(imgp.childNodes); // this must not be empty, but is empty, WHY?
+    // console.log("************ IMG-Cont")
+    // console.log(imgCont);
+    // return imgp;
     
 }
 
 
-// document.querySelector('#recompile').addEventListener('click', ()=>{
-document.querySelector('.container-markdown').addEventListener('keypress', ()=>{
-    const imgs = document.querySelectorAll('img');
+document.querySelector('#recompile').addEventListener('click', ()=>{
+    const imgs = document.querySelector('.preview').getElementsByTagName('img');
+    console.log(imgs)
 
     console.log('total images in this page:', imgs.length);
-    if(imgs.length != 0){
-         imgs.forEach(img => {
-            if(!img.classList.contains('pieces-logo')) createRibbon(img)
-         });
-    }
+    [...imgs].forEach((img, ind, imgs) => {
+           createRibbon(img, ind, imgs);
+    });
+    
 
     let md = document.querySelector('#textarea').value;
 	let html = converter.makeHtml(md);
 
+    // console.log(html)
 	document.querySelector('.preview').innerHTML = html;
 })
