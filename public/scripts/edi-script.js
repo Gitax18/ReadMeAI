780121f1b6b6f3119c7c76b2d7f1b00e389caa9c
Gitanshu sankhla <lusifer0418@gmail.com>
2023-10-15 11:14:08 +0530

    bug solved

--- public/scripts/edi-script.js
+++ public/scripts/edi-script.js
@@ -22,50 +22,23 @@ function createRibbon(img, ind, imgs){
     imgp.style.position = 'relative';
     imgp.innerHTML += ribbonHtml;
 
-    throw new Error('It works when there is error in function')
-
-    // let imgCont = document.createElement('div');
-    // imgCont.style.border = `3px solid black`;
-    // imgCont.style.height = `100%`;
-    // imgCont.style.width = `100%`;
-
-    // imgCont.innerHTML = '';
-    // imgCont.appendChild(img);
-    // imgCont.innerHTML += ribbonHtml;
-
-    // imgp.innerHTML = '';
-    // imgp.appendChild(img);
-    // imgp.innerHTML += ribbonHtml;
-
-
-    // *** Write Your code here
-
-
-    // console.log("************ IMG")
-    // console.log(img);
-    // console.log("************ IMG-P")
-    // console.log(imgp);
-    // console.log(imgp.childNodes); // this must not be empty, but is empty, WHY?
-    // console.log("************ IMG-Cont")
-    // console.log(imgCont);
-    // return imgp;
+    
     
 }
 
 
 document.querySelector('#recompile').addEventListener('click', ()=>{
-    const imgs = document.querySelector('.preview').getElementsByTagName('img');
-    console.log(imgs)
+    let md = document.querySelector('#textarea').value;
+    let html = converter.makeHtml(md);
+    document.querySelector('.preview').innerHTML = html;
 
-    console.log('total images in this page:', imgs.length);
+
+    const imgs = document.querySelector('.preview').getElementsByTagName('img');
+
+    console.log('Images and length:',imgs,  imgs.length);
     [...imgs].forEach((img, ind, imgs) => {
            createRibbon(img, ind, imgs);
     });
     
 
-    let md = document.querySelector('#textarea').value;
-	let html = converter.makeHtml(md);
-
-    // console.log(html)
-	document.querySelector('.preview').innerHTML = html;
 })
