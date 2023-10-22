9c39ee02c1b2a05a8822d023c8388e34e93fb441
dev-tashvi <tashviyadav9@gmail.com>
2023-10-22 17:22:02 +0530

    added new social button

--- public/scripts/edi-script.js
+++ public/scripts/edi-script.js
@@ -9,6 +9,7 @@ const ribbonHtml = `
 `   
 
 function createRibbon(img, ind, imgs){
+    // * Do Not Change following code
     const imgp = img.parentElement;
 
     imgs[ind] = img;
@@ -27,10 +28,25 @@ document.querySelector('#recompile').addEventListener('click', ()=>{
 
     const imgs = document.querySelector('.preview').getElementsByTagName('img');
 
+
     [...imgs].forEach((img, ind, imgs) => {
-           createRibbon(img, ind, imgs);
+
+            switch(img.alt){
+                case 'Instagram-social':
+                    img.style.height="inherit";
+                break;
+                case 'GitHub-social':
+                    img.style.height="inherit";
+                break;
+                case 'LinkedIn-social':
+                    img.style.height="inherit";
+                break;
+                default:
+                    createRibbon(img, ind, imgs);
+            }
     });
 })
+
 
 
 window.addEventListener('click',function(e){
@@ -71,11 +87,17 @@ function insertAtCursor(myField, myValue) {
     
  }
 const text = document.querySelector('#textarea');
+const sociallinks = {
+    linkdin:'[![LinkedIn-social](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/)',
+    instagram:'[![Instagram-social](https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&logo=Instagram&logoColor=white)](https://www.instagram.com/)',
+    github:'[![GitHub-social](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)](https://github.com/)'
+}
 const map = {
     'IMG':'![<alt>](<url>)\n',
-    'CODE':'```<language>\n<code comes here>\n```\n',
+    'CODE':'```<language>\nwrite your code...\n```\n',
     'LIST':'- •\n- •\n- •\n',
-    'URL':'[url tex..](https://www.markdownguide.org/basic-syntax/#code)\n'
+    'URL':'[url tex..](https://www.markdownguide.org/basic-syntax/#code)\n',
+    'SOCIAL':`${sociallinks.github}\n${sociallinks.linkdin}\n${sociallinks.instagram}`
 };
 var buttons = document.querySelectorAll('.ribbon-button')
 buttons.forEach(function(button) {
@@ -92,7 +114,6 @@ let maphead = {
     'H2':'## here your text...',
     'H3':'### here your text...'
 }
-
 select.addEventListener('change',function(){
     var selectedOption = select.options[select.selectedIndex];
     seter = maphead[selectedOption.value];
