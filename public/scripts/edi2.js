9ae76fd397b89d78855dc7d3f76bf555ad0aac45
Gitanshu sankhla <77137963+Gitax18@users.noreply.github.com>
2023-10-22 12:31:48 +0530

    just modify editor (#16)
    Co-authored-by: dev-tashvi <tashviyadav9@gmail.com>

--- public/scripts/edi2.js
+++ public/scripts/edi2.js
@@ -61,15 +61,16 @@ window.addEventListener('click',function(e){
 // function to add md template on cursor position
 function insertAtCursor(myField, myValue) {
 
-    if (myField.selectionStart || myField.selectionStart === '0') {
-         var startPos = myField.selectionStart;
-         var endPos = myField.selectionEnd;
-         myField.value = myField.value.substring(0, startPos)
-             + myValue
-             + myField.value.substring(endPos, myField.value.length);
-     } else {
-         myField.value += myValue;
-     }
+    if (myField.selectionStart || myField.selectionStart === 0) {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        myField.value = myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+    } else {
+        myField.value += myValue;
+    }
+    
  }
  
  document.querySelector('#img').addEventListener('click', ()=>{
