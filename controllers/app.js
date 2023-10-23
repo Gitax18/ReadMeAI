2ab79276882a3ed89588892467b53c53f6e6695d
Gitanshu <lusifer0418@gmail.com>
2023-10-23 18:47:11 +0530

    rename to /editor and add download feature

--- controllers/app.js
+++ controllers/app.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
+const path = require('path');
+
 const showdown = require('showdown');
 const multer = require('multer');
+const zip = require('express-zip');
 
 const palmApi = require('../api/fetchPalm');
 
 // showdown converter
 const converter = new showdown.Converter();
+converter.setFlavor('github');
 
 
 // getting template
@@ -22,19 +26,15 @@ fs.readFile('./data/template.txt', 'utf8', (err, data) => {
 // getting '/' 
 exports.getApp = (req, res)=>{
     res.render('index', {
-        pageTitle: 'DocAI Generator'
+        pageTitle: 'ReadMeAI - Upload'
     })
 }
 
 // controller to sent generate readme from incoming data
 exports.postApp = (req, res)=>{
-    console.log('incoming request', req.body);
-    console.log('incoming file', req.file);
     let html, dt;
     const code = req.file.filename;
     const description = req.body.description;
-    
-    console.log(code, description);
 
     try {
         dt = fs.readFileSync(`uploads/${code}`, 'utf8');
@@ -42,19 +42,41 @@ exports.postApp = (req, res)=>{
         console.error("read error",err);
       }
 
-    console.log('data', dt);
     palmApi.getData(template, dt, description)
         .then(data => {
             html = converter.makeHtml(data);
-
-            res.render('output', {
-                pageTitle: 'DocAI Generator',
+            res.render('editor', {
+                pageTitle: 'ReadMeAI - Editor',
                 html: html,
                 md: data
             })
-    
+            
         }).catch(err => console.log('error occured',err));
     
     //deleting files from upload folder
     fs.unlinkSync(`uploads/${code}`)
 }
+
+exports.postDownload = (req, res) => {
+    const html = req.body.html;
+    const md = req.body.markdown;
+
+    const mdFilePath = path.join(__dirname, '../downloads/readme.md');
+    const htmlFilePath = path.join(__dirname, '../downloads/readme.html');
+
+    fs.writeFile(mdFilePath, md, (err) => {
+      if (err) console.error(err);
+      else console.log('Created md file successfully');
+    });
+
+    fs.writeFile(htmlFilePath, html, (err) => {
+      if (err) console.error(err);
+      else console.log('Created html file successfully');
+    });
+
+    res.zip([
+      { path: mdFilePath, name: 'readme.md' },
+      { path: htmlFilePath, name: 'readme.html' }
+    ]);
+}
+
