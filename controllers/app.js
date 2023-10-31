1e7b52f194ff40e5a3f318ea5f9904c891f3e0c6
Gitanshu <lusifer0418@gmail.com>
2023-10-31 11:42:22 +0530

    removing files after work done

--- controllers/app.js
+++ controllers/app.js
@@ -55,12 +55,18 @@ exports.postApp = (req, res)=>{
                 pageTitle: 'ReadMeAI - Editor',
                 html: html,
                 md: data
-            })
+            });
+            //deleting files from upload folder
+            fs.unlink(`uploads/${code}`, (err) => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+                console.log('File deleted successfully');
+              });
             
         }).catch(err => console.log('error occured',err));
     
-    //deleting files from upload folder
-    fs.unlinkSync(`uploads/${code}`)
 }
 
 exports.postDownload = (req, res) => {
